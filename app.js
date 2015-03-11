2d4af9d8b6c58056af44c65e275e2456f5df34ef
Ben Milton <ben.c.milton@gmail.com>
2015-03-11 15:36:35 -0400

    Simple request response works

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+
+app.use( logger('dev') );
+
+var server = app.listen(3000, function(){
+	console.log("server listening");
+});
+
+app.get ("/" , function (req, res) {
+	res.send( 'Welcome!' )
+})
