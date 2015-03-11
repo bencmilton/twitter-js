68201b3c20276f57c1c5ec46b15ee352b9ce8df0
Ben Milton <ben.c.milton@gmail.com>
2015-03-11 18:57:47 -0400

    Tweet links working

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,23 @@ var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-    console.log(tweets);
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
+});
+
+router.get('/users/:name/tweets/:id', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  var id = req.params.id;
+  var tweetID = tweetBank.find( {id: id} );
   console.log(name);
   console.log(list);
+  console.log(tweetID);
   res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
 });
 
