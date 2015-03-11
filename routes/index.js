81a8e16ae4c6b4fe39b6496cce2c7fd062b45e50
Ben Milton <ben.c.milton@gmail.com>
2015-03-11 18:19:36 -0400

    Add a Single-User Route & View

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,16 @@ var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
+    console.log(tweets);
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+});
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  console.log(name);
+  console.log(list);
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
 });
 
 module.exports = router;
