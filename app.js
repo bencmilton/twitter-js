61bc939e7b1807db49941ef4efd71d4e8ef929a2
Ash Ryan <ash@ashryanbeats.com>
2015-03-11 17:00:37 -0400

    Passes first live test with Home.

--- app.js
+++ app.js
@@ -2,24 +2,28 @@ var express = require('express');
 var app = express();
 var logger = require('morgan');
 var swig = require('swig');
+var routes = require('./routes/');
 
 app.use( logger('dev') );
+app.use('/', routes);
+app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(3000, function(){
 	console.log("server listening");
 });
 
-app.get ("/" , function (req, res) {
-	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
-
-app.get ("/news" , function (req, res) {
-	res.send( 'Welcome to the News Page!' );
-});
 
 app.engine('html' , swig.renderFile);
 app.set('view engine' , 'html');
 app.set('views',  __dirname + '/views');
 
 swig.setDefaults({ cache: false });
+
+
+
+
+
+
+
+
+
